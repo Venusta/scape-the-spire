823e4754adebf6745895961776bd9e782c50a258
Venusta <Venusta>
2022-03-09 20:59:44 +0000

    pair helper

--- src/main/java/scapeTheSpire/util/SoundEffects.java
+++ src/main/java/scapeTheSpire/util/SoundEffects.java
@@ -6,6 +6,12 @@ import static scapeTheSpire.ScapeTheSpire.makeID;
 import static scapeTheSpire.ScapeTheSpire.makeAudioPath;
 
 public class SoundEffects {
-  public static final Pair<String, String> IceBarrageSfx = new Pair<>(makeID("IceBarrageSfx"), makeAudioPath("IceBarrage.mp3"));
-  public static final Pair<String, String> VengenceSfx = new Pair<>(makeID("VengenceSfx"), makeAudioPath("Vengence.mp3"));
+
+  public static Pair<String, String> makePair(String name) {
+    return new Pair<String, String>(makeID(name + "Sfx"), makeAudioPath(name + ".mp3"));
+  }
+
+  // method name must match "name + sfx"
+  public static final Pair<String, String> VengenceSfx = makePair("Vengence");
+  public static final Pair<String, String> IceBarrageSfx = makePair("IceBarrage");
 }
