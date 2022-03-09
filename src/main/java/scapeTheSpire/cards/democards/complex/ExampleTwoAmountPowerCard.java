6af99456cdff77ae7c1cf56a62bb6bebac575fc5
Venusta <Venusta>
2022-03-09 11:22:41 +0000

    init

---
+++ src/main/java/scapeTheSpire/cards/democards/complex/ExampleTwoAmountPowerCard.java
@@ -0,0 +1,29 @@
+package scapeTheSpire.cards.democards.complex;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import scapeTheSpire.cards.AbstractEasyCard;
+import scapeTheSpire.powers.ExampleTwoAmountPower;
+
+import static scapeTheSpire.ScapeTheSpire.makeID;
+import static scapeTheSpire.util.Wiz.applyToSelf;
+
+public class ExampleTwoAmountPowerCard extends AbstractEasyCard {
+    public final static String ID = makeID("ExampleTwoAmountPowerCard");
+    private final static int MAGIC = 1;
+    private final static int UPGRADE_MAGIC = 1;
+    private final static int COST = 1;
+
+    public ExampleTwoAmountPowerCard() {
+        super(ID, COST, CardType.POWER, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseMagicNumber = magicNumber = MAGIC;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        applyToSelf(new ExampleTwoAmountPower(p, magicNumber, 1));
+    }
+
+    public void upp() {
+        upgradeMagicNumber(UPGRADE_MAGIC);
+    }
+}
---
+++ src/main/java/scapeTheSpire/relics/TodoItem.java
@@ -0,0 +1,13 @@
+package scapeTheSpire.relics;
+
+import scapeTheSpire.TheScaper;
+
+import static scapeTheSpire.ScapeTheSpire.makeID;
+
+public class TodoItem extends AbstractEasyRelic {
+    public static final String ID = makeID("TodoItem");
+
+    public TodoItem() {
+        super(ID, RelicTier.STARTER, LandingSound.FLAT, TheScaper.Enums.SCAPER_COLOR);
+    }
+}
